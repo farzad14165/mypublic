eebab6e3c6c907fbaff260c4783424cc10b553bf
farzad14165 <115559957+farzad14165@users.noreply.github.com>
2023-07-24 12:57:37 +0200

    Add files via upload

---
+++ producer.py
@@ -0,0 +1,22 @@
+from faker import  Faker
+from kafka import KafkaProducer
+import json
+import time
+fake= Faker()
+
+def get_registered_data ():
+    return{
+	'name': fake.name (),
+	'address': fake.address(),
+	'created_at': fake.year()
+    }
+def json_serializer(data):
+	return json.dumps(data).encode('utf-8')
+
+producer= KafkaProducer (bootstrap_servers=['172.20.8.111:9092'],value_serializer=json_serializer)
+if __name__ == "__main__":
+  while 1==1:
+    registered_data=get_registered_data()
+    print (registered_data)
+    producer.send('registered_user',registered_data)
+    time.sleep(3)
